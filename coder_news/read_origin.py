6e4b2fa7b9ac0e2da87e21038964e64af925399d
BillyChen <shixuan163556@163.com>
2019-04-09 22:18:19 +0800

    init read origin

--- coder_news/read_origin.py
+++ coder_news/read_origin.py
@@ -1,10 +1,13 @@
 from django.utils.timezone import now, timedelta
 from coder_news.views import split_data
-from coder_news.models import Info
+from coder_news import models
+from django.http import HttpResponse,JsonResponse
 
 def init_read_origin(request):
     category = request.GET.get("category", '')
     category = split_data(category)
     date = now().date() + timedelta(days=-1)
+    read_num = {}
+
 
 
