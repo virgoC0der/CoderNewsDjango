47f1f6e5b9e8f92d2fff38f734ee73eebd443db4
BillyChen <shixuan163556@163.com>
2019-04-09 21:29:28 +0800

    update views.py

--- coder_news/views.py
+++ coder_news/views.py
@@ -1,8 +1,6 @@
 from coder_news import models
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.hashers import make_password, check_password
-import random
-import string
 
 
 def add_user(request):
@@ -33,19 +31,18 @@ def login(request):
 
 def find_topic(request):
     category = request.GET.get('categoryArray', '')
-    category = category.split(",")
     amount = int(request.GET.get('infoAmount', ''))
     read_info = request.GET.get('readInfo', '')
     read_num = request.GET.get('readNum', '')
-    read_info = read_info.replace('[', '')
-    read_info = read_info.replace(']', '')
-    read_info = read_info.split(",")
-    read_num = read_num.replace('[', '')
-    read_num = read_num.replace(']', '')
-    read_num = read_num.split(",")
-    print(read_info)
-    r = random.randint(1, 150)
-    print(r)
+    category = split_data(category)
+    read_info = split_data(read_info)
+    read_num = split_data(read_num)
+    read = {}
+    for info in read_info:
+        for num in read_num:
+            read[info] = int(num)
+    print(read)
+    # print(r)
     count = int(amount/len(category))
     news = {"list": []}
     i = 1
@@ -53,13 +50,17 @@ def find_topic(request):
         print(cat)
         if i == len(category):
             # print(i)
-            topic = models.Info.objects.filter(category__contains=cat)[r:amount-count*(i-1)+r].values("title", "url", "imageURL", "category")
+            topic = models.Info.objects.filter(category__contains=cat)[read[cat]:amount-count*(i-1)+read[cat]].values("title", "url", "imageURL")
         else:
-            topic = models.Info.objects.filter(category__contains=cat)[r:r+count].values("title", "url", "imageURL", "category")
+            topic = models.Info.objects.filter(category__contains=cat)[read[cat]:read[cat]+count].values("title", "url", "imageURL")
         news["list"].append(list(topic))
-        r += count
         print(topic.values("id"))
         i += 1
     return JsonResponse(news, json_dumps_params={'ensure_ascii': False}, safe=False)
-    # return HttpResponse('topic:'+topic.topic+'\nurl:'+topic.url)
 
+
+def split_data(data):
+    data = data.replace("[", '')
+    data = data.replace("]", '')
+    data = data.split(",")
+    return data
